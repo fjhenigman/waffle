b44684e44b2b9f3d4f83bc4d5a5b59ff7e88f65b
Frank Henigman <fjhenigman@google.com>
2015-11-11 22:35:34 -0500

    gbm: add gbm_device_is_format_supported
    Add gbm_device_is_format_supported() to the list of platform functions.
    
    Signed-off-by: Frank Henigman <fjhenigman@google.com>

--- src/waffle/gbm/wgbm_platform.h
+++ src/waffle/gbm/wgbm_platform.h
@@ -35,22 +35,23 @@
 #include "wcore_util.h"
 
 #define GBM_FUNCTIONS(f) \
-    f(struct gbm_device * , gbm_create_device            , (int fd)) \
-    f(int                 , gbm_device_get_fd            , (struct gbm_device *dev)) \
-    f(void                , gbm_device_destroy           , (struct gbm_device *gbm)) \
-    f(struct gbm_surface *, gbm_surface_create           , (struct gbm_device *gbm, uint32_t width, uint32_t height, uint32_t format, uint32_t flags)) \
-    f(void                , gbm_surface_destroy          , (struct gbm_surface *surface)) \
-    f(struct gbm_bo *     , gbm_surface_lock_front_buffer, (struct gbm_surface *surface)) \
-    f(void                , gbm_surface_release_buffer   , (struct gbm_surface *surface, struct gbm_bo *bo)) \
-    f(struct gbm_bo *     , gbm_bo_create                , (struct gbm_device *gbm, uint32_t width, uint32_t height, uint32_t format, uint32_t flags)) \
-    f(void                , gbm_bo_destroy               , (struct gbm_bo *bo)) \
-    f(int                 , gbm_bo_get_fd                , (struct gbm_bo *bo)) \
-    f(uint32_t            , gbm_bo_get_width             , (struct gbm_bo *bo)) \
-    f(uint32_t            , gbm_bo_get_height            , (struct gbm_bo *bo)) \
-    f(uint32_t            , gbm_bo_get_stride            , (struct gbm_bo *bo)) \
-    f(uint32_t            , gbm_bo_get_format            , (struct gbm_bo *bo)) \
-    f(union gbm_bo_handle , gbm_bo_get_handle            , (struct gbm_bo *bo)) \
-    f(struct gbm_device * , gbm_bo_get_device            , (struct gbm_bo *bo))
+    f(struct gbm_device * , gbm_create_device             , (int fd)) \
+    f(int                 , gbm_device_get_fd             , (struct gbm_device *dev)) \
+    f(void                , gbm_device_destroy            , (struct gbm_device *gbm)) \
+    f(int                 , gbm_device_is_format_supported, (struct gbm_device *gbm, uint32_t format, uint32_t usage)) \
+    f(struct gbm_surface *, gbm_surface_create            , (struct gbm_device *gbm, uint32_t width, uint32_t height, uint32_t format, uint32_t flags)) \
+    f(void                , gbm_surface_destroy           , (struct gbm_surface *surface)) \
+    f(struct gbm_bo *     , gbm_surface_lock_front_buffer , (struct gbm_surface *surface)) \
+    f(void                , gbm_surface_release_buffer    , (struct gbm_surface *surface, struct gbm_bo *bo)) \
+    f(struct gbm_bo *     , gbm_bo_create                 , (struct gbm_device *gbm, uint32_t width, uint32_t height, uint32_t format, uint32_t flags)) \
+    f(void                , gbm_bo_destroy                , (struct gbm_bo *bo)) \
+    f(int                 , gbm_bo_get_fd                 , (struct gbm_bo *bo)) \
+    f(uint32_t            , gbm_bo_get_width              , (struct gbm_bo *bo)) \
+    f(uint32_t            , gbm_bo_get_height             , (struct gbm_bo *bo)) \
+    f(uint32_t            , gbm_bo_get_stride             , (struct gbm_bo *bo)) \
+    f(uint32_t            , gbm_bo_get_format             , (struct gbm_bo *bo)) \
+    f(union gbm_bo_handle , gbm_bo_get_handle             , (struct gbm_bo *bo)) \
+    f(struct gbm_device * , gbm_bo_get_device             , (struct gbm_bo *bo)) \
 
 struct linux_platform;
 
